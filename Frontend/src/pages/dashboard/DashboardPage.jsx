946d42c9b88e007c35f86052c0747169a22967f9
Kanysh <kanyshomirzak@gmai.com>
2026-03-24 11:27:12 +0500

    Some changes

--- Frontend/src/pages/dashboard/DashboardPage.jsx
+++ Frontend/src/pages/dashboard/DashboardPage.jsx
@@ -6,7 +6,7 @@ import { MainBlock } from "@/pages/dashboard/ui/MainBlock"
 
 export function DashboardPage() {
     return (
-        <div className="flex flex-col bg-muted h-screen">
+        <div className="flex flex-col dot-bg h-screen">
             <div className="fixed top-0 left-0 right-0 z-50">
                 <Header variant="logo-left" />
             </div>
